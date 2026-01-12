0e3d259909a2d97d88beada24661398638184236
luizacosta <luiza.mcosta@mercadolivre.com>
2026-01-12 11:41:11 -0300

    Creating a panic-recover func

--- aula06-panic/clientes.go
+++ aula06-panic/clientes.go
@@ -1,23 +1,30 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"os"
 )
 
 func readTxt() {
+	//um defer para imprimir a mensagem de execução concluída
+	defer fmt.Println("Execução concluída\n")
+	
+	//e outrodefer para capturar o erro e imprimir a mensagem de erro
 	defer func() {
-		if r := recover(); r != nil {
-			fmt.Println("Execução concluída\n" , r)
+		if r := recover(); r != nil{
+			fmt.Println("Erro capturado:", r)
 		}
 	}()
-	content, err := os.ReadFile("customers.txt")
-	err = errors.New("This indicated file was not found")
 
+	//usa biblioteca "os" para ler o arquivo "customers.txt"
+	content, err := os.ReadFile("customers.txt")
+	
+	//se der erro, vai ter um panic > que vai prum recover > e vai imprimir a mensagem de erro
 	if err != nil {
-		panic(err)
+		panic("The indicated file was not found")
 	}
+
+	//imprime o conteúdo do arquivo
 	fmt.Println(string(content))
 
 }
