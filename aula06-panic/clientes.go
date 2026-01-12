edbd6fe0b65aefc8ad024ee08b0600dfad7db56f
luizacosta <luiza.mcosta@mercadolivre.com>
2026-01-12 11:51:39 -0300

    Updating panic recorver exercise

--- aula06-panic/clientes.go
+++ aula06-panic/clientes.go
@@ -8,25 +8,25 @@ import (
 func readTxt() {
 	//um defer para imprimir a mensagem de execução concluída
 	defer fmt.Println("Execução concluída\n")
-	
+
 	//e outrodefer para capturar o erro e imprimir a mensagem de erro
 	defer func() {
-		if r := recover(); r != nil{
+		if r := recover(); r != nil {
 			fmt.Println("Erro capturado:", r)
 		}
 	}()
 
 	//usa biblioteca "os" para ler o arquivo "customers.txt"
-	content, err := os.ReadFile("customers.txt")
-	
+	content, err := os.ReadFile("aula06-panic/customers.txt")
+
 	//se der erro, vai ter um panic > que vai prum recover > e vai imprimir a mensagem de erro
 	if err != nil {
 		panic("The indicated file was not found")
 	}
 
-	//imprime o conteúdo do arquivo
+	// Imprime o conteúdo
+	fmt.Println("\n📄 Conteúdo do arquivo:")
 	fmt.Println(string(content))
-
 }
 
 func main() {
